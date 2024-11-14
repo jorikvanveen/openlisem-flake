eb913b772f7fd326b655c60e497be6bbec2ffff3
Jorik <vanveenjorik@protonmail.com>
2024-11-14 12:41:30 +0100

    Update to latest main_C commit

--- flake.nix
+++ flake.nix
@@ -34,27 +34,18 @@
     packages.x86_64-linux.default = self.packages.x86_64-linux.openlisem;
     packages.x86_64-linux.openlisem = pkgs.stdenv.mkDerivation {
       pname = "openlisem";
-      version = "6.898";
+      version = "7.2";
       
       src = pkgs.fetchFromGitHub {
         owner = "vjetten";
         repo = "openlisem";
-        rev = "4b96a1e55c4be343245ac86b6e08ccde462f8166";
-        hash = "sha256-bQRQTgAT8gRC974776kfnLDC+v7T9XiQ7NrxQ0QIRdw=";
+        rev = "e19184939769eff571c4430a035f824db3679201";
+        hash = "sha256-iXjH0i5UdEe4gIsewLNIx85HyJIESHhIyOFNkkmTt+Y=";
       };
-
-      patches = [
-        ./qwt-nix.patch
-      ];
 
       env = {
         NIX_QWT = "${qwt}";
       };
-
-      cmakeFlags = [
-        "-DGDAL_INCLUDE_DIRS=${pkgs.gdal}/include"
-        "-DGDAL_LIBRARIES=${pkgs.gdal}/lib/libgdal.so"
-      ];
 
       installPhase = ''
         mkdir -p $out/bin
@@ -71,7 +62,6 @@
       nativeBuildInputs = [
         pkgs.qt6.wrapQtAppsHook
         pkgs.pkg-config
-        pkgs.gdal
         pkgs.cmake
       ];
     };
